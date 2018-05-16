84160a1394be0c15e4aae5f43fd4e338444071e7
Edward George <georgeidango@gmail.com>
2018-05-16 13:13:50 +0300

    Weather clothing rule book bean definition added.

--- src/main/java/ua/epam/javard/weatherApp/AppConfig.java
+++ src/main/java/ua/epam/javard/weatherApp/AppConfig.java
@@ -1,19 +1,34 @@
 package ua.epam.javard.weatherApp;
 
+import com.deliveredtechnologies.rulebook.model.RuleBook;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
+import ua.epam.javard.weatherApp.recommendations.WeatherClothingRuleBook;
 
 @Configuration
 @EnableScheduling
 @PropertySource("classpath:weatherbit_api.properties")
 public class AppConfig {
 
+    @Value("${temperature.neutral.upper}")
+    private int neutralTempUpperBound;
+    @Value("${temperature.neutral.lower}")
+    private int neutralTempLowerBound;
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean(name = "weatherClothingRuleBook")
+    @Scope("prototype")
+    public RuleBook book() {
+        return new WeatherClothingRuleBook(neutralTempUpperBound, neutralTempLowerBound);
     }
 }
 
